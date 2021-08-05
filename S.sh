31a4825c191269c53db03783f27da2ddfea49b84
Killarboy <87282095+Killarboy@users.noreply.github.com>
2021-08-06 01:16:15 +0300

    Add files via upload

---
+++ S.sh
@@ -0,0 +1,77 @@
+
+echo ╭╮╱╭╮|lolcat 
+echo ┃┃╱┃┃|lolcat 
+echo ┃╰━╯┣━━┳━━┳━━┳╮╱╭╮|lolcat 
+echo ┃╭━╮┃╭╮┃╭╮┃╭╮┃┃╱┃┃|lolcat 
+echo ┃┃╱┃┃╭╮┃╰╯┃╰╯┃╰━╯┃|lolcat 
+echo ╰╯╱╰┻╯╰┫╭━┫╭━┻━╮╭╯|lolcat 
+echo ╱╱╱╱╱╱╱┃┃╱┃┃╱╭━╯┃|lolcat 
+echo ╱╱╱╱╱╱╱╰╯╱╰╯╱╰━━╯|lolcat 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo ╭━━╮╱╱╱╭╮╭╮╱╱╱╭╮|lolcat 
+echo ┃╭╮┃╱╱╭╯╰┫┃╱╱╱┃┃|lolcat 
+echo ┃╰╯╰┳━╋╮╭┫╰━┳━╯┣━━┳╮╱╭╮|lolcat 
+echo ┃╭━╮┃╭╋┫┃┃╭╮┃╭╮┃╭╮┃┃╱┃┃|lolcat 
+echo ┃╰━╯┃┃┃┃╰┫┃┃┃╰╯┃╭╮┃╰━╯┃|lolcat 
+echo ╰━━━┻╯╰┻━┻╯╰┻━━┻╯╰┻━╮╭╯|lolcat 
+echo ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃|lolcat 
+echo ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯|lolcat 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo ╭━━━━╮|lolcat 
+echo ┃╭╮╭╮┃|lolcat 
+echo ╰╯┃┃┣┻━╮╭╮╱╭┳━━┳╮╭╮|lolcat 
+echo ╱╱┃┃┃╭╮┃┃┃╱┃┃╭╮┃┃┃┃|lolcat 
+echo ╱╱┃┃┃╰╯┃┃╰━╯┃╰╯┃╰╯┃|lolcat 
+echo ╱╱╰╯╰━━╯╰━╮╭┻━━┻━━╯|lolcat 
+echo ╱╱╱╱╱╱╱╱╭━╯┃|lolcat 
+echo ╱╱╱╱╱╱╱╱╰━━╯|lolcat 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo
+echo ╭━━━╮╱╱╱╱╱╱╱╱╭╮|lolcat 
+echo ┃╭━╮┃╱╱╱╱╱╱╱╱┃┃|lolcat 
+echo ┃╰━━╮╭━━╮╭╮╭━╯┃╭━━╮|lolcat 
+echo ╰━━╮┃┃╭╮┃┣┫┃╭╮┃┃╭╮┃|lolcat 
+echo ┃╰━╯┃┃╭╮┃┃┃┃╰╯┃┃╭╮┃|lolcat 
+echo ╰━━━╯╰╯╰╯╰╯╰━━╯╰╯╰╯|lolcat 
+echo 
+echo 
+echo ╱╱╱╱╱╭━╮|lolcat 
+echo ╱╱╱╱╱┃╭╯|lolcat 
+echo ╭━━╮╭╯╰╮╭━╮╭╮╭━╮|lolcat 
+echo ┃╭╮┃╰╮╭╯┃╭╯┣┫┃╭╮╮|lolcat 
+echo ┃╭╮┃╱┃┃╱┃┃╱┃┃┃┃┃┃|lolcat 
+echo ╰╯╰╯╱╰╯╱╰╯╱╰╯╰╯╰╯|lolcat 
+echo
+echo 
+echo ╱╱╱╱╭╮╱╱╭╮|lolcat 
+echo ╱╱╱╱┃┃╱╱┃┃|lolcat 
+echo ╭━━╮┃┃╭╮┃╰━╮╭╮|lolcat 
+echo ┃╭╮┃┃╰╯╯┃╭╮┃┣┫|lolcat 
+echo ┃╭╮┃┃╭╮╮┃┃┃┃┃┃|lolcat 
+echo ╰╯╰╯╰╯╰╯╰╯╰╯╰╯|lolcat 
+echo 
+echo 
+echo '𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆 :- 𝗔𝗦 𝗦𝗼𝘄𝗿𝗼𝘃' |lolcat 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
